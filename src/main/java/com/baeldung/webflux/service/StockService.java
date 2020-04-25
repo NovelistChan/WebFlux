be44121f58acab058d7e77fff11b25c7cff9e4f7
NovelistChan <774527381@qq.com>
2020-04-26 00:48:23 +0800

    update

--- src/main/java/com/baeldung/webflux/service/StockService.java
+++ src/main/java/com/baeldung/webflux/service/StockService.java
@@ -30,7 +30,7 @@ public class StockService {
 
 
     public Flux<NewStock> streamStock() {
-        System.setProperty("selenuim_config", "/Users/njuics/workspace/Spring-WebFlux/spring-webflux-streaming/config.ini");
+        System.setProperty("selenuim_config", "/Webflux/config.ini");
         Flux<Long> interval = Flux.interval(Duration.ofSeconds(5));
         FluxProcessor events = DirectProcessor.create().serialize();
         FluxSink sink = events.sink();
@@ -67,7 +67,7 @@ public class StockService {
                         return 0;
                     }
                 }))
-                .setDownloader(new NewSeleniumDownloader("/Users/njuics/Downloads/chromedriver")).thread(5).start();
+                .setDownloader(new NewSeleniumDownloader("/Webflux/chromedriver")).thread(5).start();
 
         return events;
     }
